16fdc11cb076edf5c2d6dd5dd596ade1493deb99
pecesama <pecesama@gmail.com>
2007-05-12 03:44:12 +0000

    adding the new control panel (3)

--- admin/admin.php
+++ admin/admin.php
@@ -19,40 +19,40 @@ $conf = new configuration();
 if ($user->isAdmin()) {
 	
 ?>
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>gelato</title>
+		<title>gelato :: admin users</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="shortcut icon" href="<?=$conf->urlGelato;?>/images/favicon.ico" />
 		<script language="javascript" type="text/javascript" src="<?=$conf->urlGelato;?>/admin/scripts/mootools.js"></script>
+		<script language="javascript" type="text/javascript" src="<?=$conf->urlGelato;?>/admin/scripts/sortable.js"></script>
 		<style type="text/css" media="screen">	
-			@import "<?=$conf->urlGelato;?>/admin/css/style-codice.css";
+			@import "<?=$conf->urlGelato;?>/admin/css/style.css";
 		</style>
 	</head>
+	
 	<body>
 		<div id="div-process" style="display:none;">Processing request...</div>
-		<div id="titulo">
-			<img src="<?=$conf->urlGelato;?>/images/logo.jpg" alt="gelato CMS" title="gelato CMS" />	
-		</div>
-		
-		<div id="menuContenedor">
-			<ul>
-				<li id="active"><a href="#" id="current">Users</a></li>
-					<ul>
-						<li><a href="user.php">Add</a></li>
-						<li id="subactive"><a href="#" id="subcurrent">Manage</a></li>
+		<div id="cont">
+			<div id="head">
+				<h1><a href="<?=$conf->urlGelato;?>/admin/index.php" title="gelato :: home">gelato cms</a></h1>
+				<ul id="nav">
+					<li><a href="<?=$conf->urlGelato;?>/" title="Take me to the tumblelog">View Tumblelog</a></li>
+					<li><a href="close.php" title="Log off" onclick="return exit('div-process','<?=$conf->urlGelato;?>/admin/ajax.php?action=close');">Log out</a></li>
+			  	</ul>
+			</div>
+			<div id="main">				
+				
+				<div class="box">
+					<ul class="menu manage">
+					<h3>Manage</h3>
+					<li><a href="#">Settings</a></li>
+					<li><a href="<?=$conf->urlGelato;?>/admin/index.php">Posts</a></li>
+					<li><a href="<?=$conf->urlGelato;?>/admin/user.php">Add user</a></li>
+					<li class="selected"><a href="#">Users</a></li>
 					</ul>
-				</li>
-				<li><a href="index.php">Control Panel</a></li>
-			</ul>
-		</div>
-		
-		<div id="contenido">
-			<div class="center">
-				<div  class="ventana">
-					<p class="titulo"><span class="handle">Manage the user information</span></p>
-					<div id="formulario">
+
 <?					
 						if (isset($_GET["added"])) {
 							if ($_GET["added"]=="true") {
@@ -80,60 +80,57 @@ if ($user->isAdmin()) {
 							}
 						}
 ?>						
-						<p>
-							<table class="sortable" id="admin-tabla">
-								<caption>Authors list.</caption>
-								<thead>
-									<tr>
-										<th scope="col">Login</th>
-										<th scope="col">Name</th>
-										<th colspan="2" scope="col" class="unsortable">Actions</th>
-									</tr>
-								</thead>
-								<tbody>
-<?	
-						$odd=false;
-						$rs = $user->getUsers();
-						if ($user->contarRegistros()>0) {
-							while($register = mysql_fetch_array($rs)) {
-?>
-								<tr <? if ($odd) { echo 'class="odd"'; } $odd=!$odd; ?>>
-									<td>
-										<? echo $register["login"]."\n"; ?>
-									</td>
-									<td>
-										<? echo $register["name"]."\n"; ?>
-									</td>
-									<td>
-										<a href="user.php?edit=<?=$register["id_user"]; ?>">Edit</a>
-									</td>
-									<td>
-										<a href="user.php?delete=<?=$register["id_user"]; ?>">Delete</a>	
-									</td>
+						<div class="tabla">
+						<table class="sortable" id="admin-table">
+							<thead>
+								<tr>
+									<th scope="col">Login</th>
+									<th scope="col">Name</th>
+									<th colspan="2" scope="col" class="unsortable">Actions</th>
 								</tr>
-<?
-							}
-						}
-						else {
+							</thead>
+							<tbody>
+<?	
+					$odd=false;
+					$rs = $user->getUsers();
+					if ($user->contarRegistros()>0) {
+						while($register = mysql_fetch_array($rs)) {
 ?>
-							<tr> 
-								<td colspan="4"><div class="exito">No users available.</div></td>
+							<tr <? if ($odd) { echo 'class="odd"'; } $odd=!$odd; ?>>
+								<td>
+									<? echo $register["login"]."\n"; ?>
+								</td>
+								<td>
+									<? echo $register["name"]."\n"; ?>
+								</td>
+								<td>
+									<a href="user.php?edit=<?=$register["id_user"]; ?>">Edit</a>
+								</td>
+								<td>
+									<a href="user.php?delete=<?=$register["id_user"]; ?>">Delete</a>	
+								</td>
 							</tr>
 <?
 						}
+					}
+					else {
 ?>
-								</tbody>
-							</table>
-						</p>
-						<p>&nbsp;</p>
+						<tr> 
+							<td colspan="4"><div class="exito">No users available.</div></td>
+						</tr>
+<?
+					}
+?>
+							</tbody>
+						</table>
 					</div>
-				</div> 
-			</div> 		
-			<div id="pie">
-				<p>
-					<a href="http://www.gelatocms.com/" title="gelato CMS" target="_blank">gelato CMS</a> :: PHP/MySQL Tumblelog Content Management System.
-				</p>
-			</div>				
+
+					<div class="footer-box">&nbsp;</div>
+				</div>
+			</div>
+			<div id="foot">
+				<a href="http://www.gelatocms.com/" title="gelato CMS" target="_blank">gelato CMS</a> :: PHP/MySQL Tumblelog Content Management System.
+			</div>
 		</div>
 	</body>
 	</html>
