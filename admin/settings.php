2f150108c308e363bbd0db6c78038ce37c40eaa6
Victor De la Rocha <vyk2rr@gmail.com>
2007-07-20 08:14:07 +0000

    added __("text") function for the gettext class

--- admin/settings.php
+++ admin/settings.php
@@ -32,7 +32,7 @@ if ($user->isAdmin()) {
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>gelato :: settings</title>
+		<title>gelato :: <?=__("settings")?></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="generator" content="gelato cms <?php echo version();?>" />
 		<link rel="shortcut icon" href="<?php echo $conf->urlGelato;?>/images/favicon.ico" />
@@ -55,34 +55,34 @@ if ($user->isAdmin()) {
 	</head>
 	
 	<body>
-		<div id="div-process" style="display:none;">Processing request...</div>
+		<div id="div-process" style="display:none;"><?=__("Processing request...")?></div>
 		<div id="cont">
 			<div id="head">
-				<h1><a href="<?php echo $conf->urlGelato;?>/" title="gelato :: home">gelato cms</a></h1>
+				<h1><a href="<?php echo $conf->urlGelato;?>/" title="gelato :: <?=__("home")?>">gelato cms</a></h1>
 				<ul id="nav">
-					<li><a href="<?php echo $conf->urlGelato;?>/" title="Take me to the tumblelog">Back to the Tumblelog</a></li>
+					<li><a href="<?php echo $conf->urlGelato;?>/" title="<?=__("Take me to the tumblelog")?>"><?=__("Back to the Tumblelog")?></a></li>
 			  	</ul>
 			</div>
 			<div id="main">				
 				
 				<div class="box">
 					<ul class="menu manage">
-					<h3>Tumblelog configuration</h3>
-					<li><a href="index.php">Post</a></li>
-					<li><a href="admin.php">Users</a></li>
-					<li><a href="options.php">Options</a></li>
-					<li class="selected"><a>Settings</a></li>
+					<h3><?=__("Tumblelog configuration")?></h3>
+					<li><a href="index.php"><?=__("Post")?></a></li>
+					<li><a href="admin.php"><?=__("Users")?></a></li>
+					<li><a href="options.php"><?=__("Options")?></a></li>
+					<li class="selected"><a><?=__("Settings")?></a></li>
 					</ul>
 					<p>&nbsp;</p>
 <?php
 					if (isset($_GET["modified"])) {
 						if ($_GET["modified"]=="true") {
-							echo "<div class=\"exito\" id=\"divMessages\">The configuration has been modified successfully.</div>";
+							echo "<div class=\"exito\" id=\"divMessages\">".__("The configuration has been modified successfully.")."</div>";
 						}
 					}					
 					if (isset($_GET["error"])) {
 						if ($_GET["error"]==1) {
-							echo "<div class=\"error\" id=\"divMessages\"><strong>Error on the database server: </strong>".$_GET["des"]."</div>";
+							echo "<div class=\"error\" id=\"divMessages\"><strong>".__("Error on the database server: ")."</strong>".$_GET["des"]."</div>";
 						}
 					}
 ?>
@@ -91,20 +91,20 @@ if ($user->isAdmin()) {
 						<form action="settings.php" method="post" id="settings_form" autocomplete="off" class="newpost">							
 							<fieldset>								
 								<ul>							
-									<li><label for="title">Title:</label>
+									<li><label for="title"><?=__("Title:")?></label>
 										<input type="text" name="title" id="title" value="<?php echo $conf->title;?>" class="txt"/></li>
-									<li><label for="description">Description:</label>
+									<li><label for="description"><?=__("Description:")?></label>
 										<input type="text" name="description" id="description" value="<?php echo $conf->description;?>" class="txt"/></li>
-									<li><label for="url_installation">Installation URL</label>
+									<li><label for="url_installation"><?=__("Installation URL")?></label>
 										<input type="text" name="url_installation" id="url_installation" value="<?php echo $conf->urlGelato;?>" class="txt"/></li>
-									<li><label for="posts_limit">Post limit:</label>
+									<li><label for="posts_limit"><?=__("Post limit:")?></label>
 										<input type="text" name="posts_limit" id="posts_limit" value="<?php echo $conf->postLimit;?>" class="txt"/></li>
-									<li><label for="lang">Language:</label>
+									<li><label for="lang"><?=__("Language:")?></label>
 										<select id="lang" name="lang">
 											<option value="en" selected="selected">english</option>
 										</select>
 									</li>
-									<li><label for="template">Template:</label>
+									<li><label for="template"><?=__("Template:"?></label>
 										<select id="template" name="template">
 <?php
 										$themes = getThemes();
@@ -119,7 +119,7 @@ if ($user->isAdmin()) {
 								</ul>
 							</fieldset>
 							<p>
-								<input type="submit" name="btnsubmit" id="btnsubmit" value="Modify" class="submit"/>
+								<input type="submit" name="btnsubmit" id="btnsubmit" value="<?=__("Modify")?>" class="submit"/>
 							</p>
 						</form>	
 								
