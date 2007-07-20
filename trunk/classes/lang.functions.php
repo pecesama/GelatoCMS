ffd3f3d3978e02ed7d91e6863daaf3817197d9d1
vyk2rr <vyk2rr@5b89ad25-372c-0410-87af-59d4b192d990>
2007-07-20 05:24:11 +0000

    change the name of files in classes for gettext (multi languages)

--- trunk/classes/lang.functions.php
+++ trunk/classes/lang.functions.php
@@ -1,28 +1,11 @@
-<?
-$idiomas = array();
-$idiomas['es_MX'] = "Espa&ntilde;ol de M&eacute;xico";
-$idiomas['en'] = "English";
-
-function esIdioma($test) {
-	global $idiomas;
-	if (isset($idiomas[$test]))
-		return true;
-	else
-		return false;
-}
-
+<?php
 function initIdioma($lang = "en") {
 	global $locale, $l10n;
-
-	if (!esIdioma($locale) && esIdioma($lang)) {
-		$locale = $lang;
-	}
 
 	$input = new FileReader(dirname(__FILE__)."/../idiomas/". $locale ."/lang.mo");
 	$l10n = new gettext_reader($input);
 }
 
-// Standard wrappers for xgettext
 function __($text) {
 	global $l10n;
 	return $l10n->translate($text);
