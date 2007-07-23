9a7f627a5e470dd8ebbedaa78b7191279b24e7bc
pecesama <pecesama@5b89ad25-372c-0410-87af-59d4b192d990>
2007-07-23 02:49:26 +0000

    scape the login

--- trunk/classes/functions.php
+++ trunk/classes/functions.php
@@ -327,4 +327,16 @@
 		$dirs = array_unique($dirs);
  		return $dirs;
  	}
+	
+	function sql_escape($value) {
+	    if(get_magic_quotes_gpc()) {
+	          $value = stripslashes($value);
+	    }
+	    if( function_exists("mysql_real_escape_string")) {
+	          $value = mysql_real_escape_string($value);
+	    } else {
+	          $value = addslashes($value);
+	    }
+	    return $value;
+	}
 ?>
