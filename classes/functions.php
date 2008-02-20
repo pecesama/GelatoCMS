80dcdc66ba0f3543d2d01d4446b6ee1d3cc9820f
vbracco <vbracco@gmail.com>
2008-02-20 19:08:40 +0000

    Removing deprecated tags for xhtml and removing invalid tags from Title post.

--- classes/functions.php
+++ classes/functions.php
@@ -395,8 +395,12 @@ if(!defined('entry') || !entry) die('Not a valid page');
  		return $dirs;
  	}
 
-	function removeBadTags($source) {
-		$validTags ='<p><ol><ul><li><a><abbr><acronym><blockquote><code><pre><em><i><strike><s><strong><b><br><span><div><img>';
+	function removeBadTags($source,$secure=false) {
+		if($secure){
+			$validTags ='<abbr><acronym><em><i><strong><b><span>';
+		} else {
+			$validTags ='<p><ol><ul><li><a><abbr><acronym><blockquote><code><pre><em><i><strong><b><br><span><div><img>';
+		}
 		$source = strip_tags($source, $validTags);
 		return preg_replace('/<(.*?)>/ie', "'<'.removeBadAtributes('\\1').'>'", $source);
 	}
