911f12269bf552ed5bd35fdd779696a203331044
pecesama <pecesama@5b89ad25-372c-0410-87af-59d4b192d990>
2008-10-24 05:58:24 +0000

    Fixed a bug with admin login

--- trunk/entry.php
+++ trunk/entry.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
  if(!defined('entry') || !entry) die('Not a valid page');
 /*
  * Created on Sep 15, 2007
@@ -24,6 +25,10 @@
  * classes/imgsize.php
  */
 
+error_reporting (E_ALL);
+ini_set('display_errors', '1');
+
+ 
 // PHP settings specific to Gelato
 ini_set('pcre.backtrack_limit', '10000');
 // Globals to be used throughout the application
@@ -31,12 +36,14 @@ define('Absolute_Path', dirname(__FILE__).DIRECTORY_SEPARATOR);
 $installed = true;
 $configFile = Absolute_Path.'config.php';
 
-if(strpos($_SERVER['REQUEST_URI'],'/admin'))$dir = "../"; else $dir = "";
+$dir = (strpos($_SERVER['REQUEST_URI'],'/admin')) ? "../" : "";
+
 if (!file_exists($configFile) and basename($_SERVER['PHP_SELF'])!='install.php'){
-	header("Location: {$dir}install.php");exit;
-}else{
-	include(Absolute_Path.'classes/install.class.php');
-	$install = new Install();
+	header("Location: {$dir}install.php");
+	exit;
+} else {	
+	include(Absolute_Path.'classes/install.class.php');	
+	$install = new Install();	
 	if(!$install->is_gelato_installed()){
 		if(basename($_SERVER['PHP_SELF'])!='install.php'){
 				header("Location: {$dir}install.php");exit;
@@ -45,8 +52,9 @@ if (!file_exists($configFile) and basename($_SERVER['PHP_SELF'])!='install.php')
 	}
 }
 
-if($installed)
+if($installed) {
 	require($configFile);
+}
 
 require_once("classes/configuration.class.php");
 require_once("classes/textile.class.php");
@@ -62,16 +70,18 @@ require_once(Absolute_Path.'classes'.DIRECTORY_SEPARATOR.'gettext.class.php');
 require_once(Absolute_Path.'classes'.DIRECTORY_SEPARATOR.'lang.functions.php');
 
 if($installed){
+
 	// Globals to be used throughout the application
 	$user = new user();
 	$tumble = new gelato();
 	$conf = new configuration();
 	$db = new Conexion_Mysql(DB_name, DB_Server, DB_User, DB_Password);
-
+	
 	session_start();
-
-	$feeds = new feeds();
-	$feeds->updateFeeds();
+	
+	$feeds = new feeds();	
+	$feeds->updateFeeds();	
 	unset($feeds);
 }
 ?>
+
