fd69f73e6651dd8d4d7f1b8539652ca6072e1cc5
vbracco <vbracco@gmail.com>
2008-10-30 20:09:35 +0000

    some modifications

--- gettext.bat
+++ gettext.bat
@@ -1,24 +1,17 @@
-dir /a /b /-p /o:gen *.php > sources.txt
-cd admin
-dir /a /b /-p /o:gen *.php > sources_admin.txt
-cd ..
+dir /a /b /-p /s /o:gen *.php > sources_php.txt
  
+PATH C:\Program Files\poEdit\bin
 PATH C:\Archivos de programa\poEdit\bin
- 
+
 for /f %%a in ('dir /b languages') do call :add_strings "%%a"
  
-cd admin
-del sources_admin.txt
-cd ..
-del sources.txt
+del sources_php.txt
  
 goto :eof
  
 :add_strings
-xgettext --keyword=__ --language=PHP --files-from=sources.txt -j --from-code=UTF-8 -d languages/%1/messages
-cd admin
-xgettext --keyword=__ --language=PHP --files-from=sources_admin.txt -j --from-code=UTF-8 -d ../languages/%1/messages
-cd ..
+xgettext --keyword=__ --language=PHP --package-name=gelatocms --package-version=1.0 --no-location --no-wrap --files-from=sources_php.txt -j --from-code=UTF-8 -d languages/%1/messages
+
 cd languages/%1
 msgfmt messages.po
 cd ../..
