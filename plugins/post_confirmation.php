c3685083f38f7c90b1149ef55b1343cc1bf6eb9f
vbracco <vbracco@gmail.com>
2008-10-31 20:25:10 +0000

    added PostConfirmation plugin

---
+++ plugins/post_confirmation.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: PostConfirmation
+Plugin URI: http://www.gelatocms.com/plugins/postconfirmation/
+Description: Add a confirmation dialog when submit the Create-post button.
+Author: Victor Bracco
+Author URI: http://www.vbracco.com.ar/
+Version: 1.0
+*/ 
+
+class post_confirmation extends plugins {
+
+	function post_confirmation(){
+		$this->addAction('admin_includes', 'post_confirmation_script');
+	}
+	
+	function post_confirmation_script() {
+		global $admin_includes,$isEdition;
+		if(!$isEdition){
+			$admin_includes .= "
+			<script type=\"text/javascript\">	
+			$(document).ready(function(){
+				$('#publish').click( function(){
+					return (confirm('".__("Do you realy want to publish this?")."'));
+				});
+			});
+			</script>
+			";
+		}
+	}
+}
+?>
