8330c4613b849901c58c50a32fd98ec360e2f9e4
pecesama <pecesama@gmail.com>
2008-11-08 07:18:35 +0000

    Updated to sitemap protocol 0.9 Implementation of getPermalink on rss.php

--- sitemap.php
+++ sitemap.php
@@ -12,54 +12,46 @@ if(!defined('entry')) define('entry',true);
   =========================== */
 ?>
 <?php
+	require('entry.php');	
+	global $conf, $tumble, $db;
+	
 	// Code modified from the one at http://paste.ubuntu-nl.org/44548/
-	header("Content-type: text/xml; charset=utf-8");	
-
-	require(dirname(__FILE__)."/config.php");
-	include("classes/configuration.class.php");
-	include("classes/gelato.class.php");
-	$tumble = new gelato();
-	$conf = new configuration();	
+	header("Content-type: text/xml; charset=utf-8");
 	$isFeed = true;
 	
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	echo "<?xml-stylesheet type=\"text/xsl\" href=\"".$conf->urlGelato."/sitemap.xsl\"?>\n";
 	
 	$rs = $tumble->getPosts(1);	
 ?>
 <!-- generator="gelato CMS" -->
-
-<urlset 
-		xmlns="http://www.google.com/schemas/sitemap/0.84"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.google.com/schemas/sitemap/0.84
-        	http://www.google.com/schemas/sitemap/0.84/sitemap.xsd">
-	
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">	
 	<url>
-		<loc><?php echo $conf->urlGelato;?></loc>
-        <priority>0.9</priority>		
+		<loc><?php echo $conf->urlGelato;?></loc>        		
 <?php
-		if ($tumble->contarRegistros()>0) {
+		if ($db->contarRegistros()>0) {
 			$register = mysql_fetch_array($rs);
 			$formatedDate = gmdate("Y-m-d", strtotime($register["date"]));
-			echo "<lastmod>".$formatedDate."</lastmod>";
+			echo "\n\t<lastmod>".$formatedDate."</lastmod>";
 		}
 ?>
-		<changefreq>hourly</changefreq>        
+		<changefreq>hourly</changefreq>
+        <priority>0.9</priority>
 	</url>
 
 
 <?php	
 	$rs = $tumble->getPosts($tumble->getPostsNumber());
-	if ($tumble->contarRegistros()>0) {
-		while($register = mysql_fetch_array($rs)) {
-			$url = htmlspecialchars($conf->urlGelato."/index.php?post=".$register["id_post"]);
+	if ($db->contarRegistros()>0) {
+		while($register = mysql_fetch_array($rs)) {			
+			$url = $tumble->getPermalink($register["id_post"]);
 			$formatedDate = gmdate("Y-m-d", strtotime($register["date"]));
 ?>
 	<url>
-		<loc><?php echo $url;?></loc>
-        <priority>0.7</priority>
+		<loc><?php echo $url;?></loc>        
 		<lastmod><?php echo $formatedDate;?></lastmod>
-        <changefreq>daily</changefreq>        
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
 	</url>
 <?php
 		}
